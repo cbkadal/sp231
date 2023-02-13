75c5fa2cc8b30f2d9aecefa534db845fc3111e06
Cicak Bin Kadal <70104051+cbkadal@users.noreply.github.com>
2023-02-13 08:50:06 +0700

    new file:   Gemfile SandBox/cbkadal.tar.xz _config.yml favicon.ico index.md modified:   README.md TXT/SHA256SUM modified:   TXT/SHA256SUM.asc modified:   TXT/myscript.sh

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
-# os231
+---
+---
 
-[GitHub](https://github.com/cbkadal/sp231/) | [LOG](TXT/mylog.txt)
+# sp231
+
+[GitHub](https://github.com/cbkadal/sp231/) | | [LOG](TXT/mylog.txt) | | [TARBALL](SandBox/cbkadal.tar.xz)
+
+## May the fork() be with you!
+
+#### Rev 006 --- 13 Feb 2023
+
--- TXT/myscript.sh
+++ TXT/myscript.sh
@@ -7,6 +7,7 @@ WEEK="00"
 # useful, but WITHOUT ANY WARRANTY; without even the implied 
 # warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 
+# REV29: Mon 13 Feb 2023 08:38:11 WIB
 # REV19: Sun 05 Feb 2023 20:00
 # REV11: Sun 08 May 2022 06:00
 # REV08: Sun 13 Mar 2022 23:00
@@ -18,28 +19,88 @@ WEEK="00"
 # You new to set "REC2" with your own Public-Key Identity!
 # Check it out with "gpg --list-key"
 
+# ATTN:
+# You new to set "REC2" with your own Public-Key Identity!
+# Check it out with "gpg --list-key"
+# ####################### Replace REC2 ####
 REC2="60914D29C01C81F1"
+# ####################### ####### #### ####
+# REC1: public key
 REC1="63FB12B215403B20"
+# WEEKURL="http://localhost:4000/WEEK/WEEK.txt"
+WEEKURL="https://sp.vlsm.org/WEEK/WEEK.txt"
 FILES="my*.asc my*.txt my*.sh"
 SHA="SHA256SUM"
 RESDIR="$HOME/SP_RESULT/"
+usage()  { echo "Usage: $0 [-w <WEEK>]" 1>&2; exit 1; }
+nolink() { echo "No LINK $1"            1>&2; exit 1; }
 
+# Check current WEEK
+unset WEEK DEFAULT
+if [ ! -z "${1##*[!0-9]*}" ] ; then
+  WEEK=$1
+elif [ -z $1 ] ; then
+  DEFAULT=1
+else while getopts ":w:W:" varTMP
+  do
+    case "${varTMP}" in
+     w|W)
+       WEEK=${OPTARG}
+       [ ! -z "${WEEK##*[!0-9]*}" ] || usage ;;
+    esac
+  done
+  [ -z $WEEK ] && usage
+fi
+
+if [ $DEFAULT ] ; then
+  [[ $(wget $WEEKURL -O- 2>/dev/null) ]] || nolink $WEEKURL
+  intARR=($(wget -q -O - $WEEKURL | awk '/\| Week / { 
+    cmd = "date -d " $2 " +%s"
+    cmd | getline mydate
+    close(cmd)
+    print mydate + (86400 * 6)
+  }'))
+  DATE=$( LANG=en_us_8859_1;date -d $(date +%d-%b-%Y) +%s)
+  for II in ${!intARR[@]} ; do
+    (( $DATE > ${intARR[$II]} )) || break;
+  done
+  WEEK=$II
+  # echo "DEBUG:TMP:$DEFAULT:W[$WEEK]:$1:$DATE:"
+fi
+
+(( WEEK > 11 )) && WEEK=11
+WEEK=$(printf "W%2.2d\n" $WEEK)
+
+# echo $WEEK ; exit
+
+# Is this the correct WEEK?
+read -r -p "Is this WEEK $WEEK ? [y/N] " response
+case "$response" in
+    [yY][eE][sS]|[yY]) 
+        ;;
+    *)
+        echo "It is not Week $WEEK!"
+        exit 1
+        ;;
+esac
+
+# TXT
 [ -d $RESDIR ] || mkdir -p $RESDIR
 pushd $RESDIR
 for II in W?? ; do
     [ -d $II ] || continue
-    TARFILE=my$II.tar.xz
+    TARFILE=my$II.tar.bz2
     TARFASC=$TARFILE.asc
     rm -vf $TARFILE $TARFASC
-    echo "tar cfJ $TARFILE $II/"
-    tar cfJ $TARFILE $II/
+    echo "tar cfj $TARFILE $II/"
+    tar cfj $TARFILE $II/
     echo "gpg --armor --output $TARFASC --encrypt --recipient $REC1 --recipient $REC2 $TARFILE"
     gpg --armor --output $TARFASC --encrypt --recipient $REC1 --recipient $REC2 $TARFILE
 done
 popd
 
-if [[ "$WEEK" != "00" ]] ; then
-    II="${RESDIR}myW$WEEK.tar.bz2.asc"
+if [[ "$WEEK" != "W00" ]] && [[ "$WEEK" != "W01" ]] ; then
+    II="${RESDIR}my$WEEK.tar.bz2.asc"
     echo "Check and move $II..."
     [ -f $II ] && mv -vf $II .
 fi
@@ -50,18 +111,21 @@ rm -f $SHA $SHA.asc
 echo "sha256sum $FILES > $SHA"
 sha256sum $FILES > $SHA
 
+echo "# ################ CHECKSUM ###### #########"
 echo "sha256sum -c $SHA"
 sha256sum -c $SHA
 
+echo "# ################# SIGNING CHECKSUM ######### ######### ########"
 echo "gpg --output $SHA.asc --armor --sign --detach-sign $SHA"
 gpg --output $SHA.asc --armor --sign --detach-sign $SHA
 
+echo "# ################# VERIFY ######### ######### ######### ########"
 echo "gpg --verify $SHA.asc $SHA"
 gpg --verify $SHA.asc $SHA
 
 echo ""
 echo "==== ==== ==== ==== ==== ==== ==== ==== ==== ==== ==== ===="
-echo "==== ==== ==== ATTN: is this WEEK $WEEK ?? ==== ==== ==== ===="
+echo "==== ==== ==== ATTN: is this WEEK $WEEK ?? === ==== ==== ===="
 echo "==== ==== ==== ==== ==== ==== ==== ==== ==== ==== ==== ===="
 echo ""
 
